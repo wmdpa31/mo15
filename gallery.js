3800a9da955b42f355f06b34413dfede777323d8
소우주 <sowoojoo@soujuui-MacBookPro.local>
2024-08-15 23:43:15 +0900

    수정

--- gallery.js
+++ gallery.js
@@ -48,7 +48,7 @@ window.onload = () => {
       content: {
         title: "김준영 ♥ 박민주의 모바일 청첩장",
         description: "결혼식에 초대합니다♡",
-        imageUrl: "https://i.imgur.com/BlGwq8f.png",
+        imageUrl: "https://i.imgur.com/tYuIfbQ.png",
         imageWidth: 1200,
         imageHeight: 630,
         link: {
